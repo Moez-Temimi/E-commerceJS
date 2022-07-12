d5d35efb771b55fb7ba6a8485b8e2618ac8b55a9
Moez-Temimi <moez.temimi@yahoo.com>
2022-07-12 11:40:08 +0200

    firstProject

--- main.js
+++
@@ -1,5 +0,0 @@
-var div = document.getElementsByTagName("div")
-	function displayInformation(product, id){
-		return ("div #id").innerHTML = product.brand \n product.type \n product.weight \n product.price 
-
-	}
